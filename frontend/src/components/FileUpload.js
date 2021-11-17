56d9955680cf2d9ae7d20aa0f449bec2dd99c7f8
c-a-ray <c.a.ray@outlook.com>
2021-11-16 16:52:23 -0800

    Style file upload page

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,32 +1,46 @@
 import React from "react";
 import ContinueButton from "./ContinueButton";
+import {
+  Card,
+  CardBody,
+  CardHeader
+} from "reactstrap";
 
 function FileUpload(props) {
   return (
     <div>
-      <input type="file" name="file" onChange={props.selectFile} />
-      {props.isSelected ? (
-        <div>
-          <p>Filename: {props.selectedFile.name}</p>
-          <p>Filetype: {props.selectedFile.type}</p>
-          <p>Size in bytes: {props.selectedFile.size}</p>
-        </div>
-      ) : (
-        <p>Select a CSV file to transform</p>
-      )}
-      <label>
-        <input
-          type="checkbox"
-          name="hasHeader"
-          checked={props.hasHeader}
-          onChange={props.handleCheckboxChange}
-        />
-        CSV has header row
-      </label>
-
-      <div>
-        <ContinueButton handleSubmission={props.uploadFile} />
-      </div>
+      <span>
+        <Card>
+          <CardHeader>Select a file to load</CardHeader>
+          <CardBody>
+            <input type="file" name="file" onChange={props.selectFile} />
+          </CardBody>
+          <CardBody>
+            {props.isSelected ? (
+              <div>
+                <p>Filetype: {props.selectedFile.type}</p>
+                <p>Size in bytes: {props.selectedFile.size}</p>
+              </div>
+            ) : (
+              null
+            )}
+          </CardBody>
+          <CardBody>
+            <label>
+              <input
+                type="checkbox"
+                name="hasHeader"
+                checked={props.hasHeader}
+                onChange={props.handleCheckboxChange}
+              />
+              CSV has header row
+            </label>
+          </CardBody>
+          <CardBody>
+              <ContinueButton handleSubmission={props.uploadFile} />
+          </CardBody>
+        </Card>
+      </span>
     </div>
   );
 }
